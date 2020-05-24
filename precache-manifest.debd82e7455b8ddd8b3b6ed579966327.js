397dd8995dc3574d08531d39a5cccb0cd424bbfe
Agustin Zamar <agustinzamar33@gmail.com>
2020-05-24 20:55:05 -0300

    Updates

---
+++ precache-manifest.debd82e7455b8ddd8b3b6ed579966327.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34051c232445a14fa038e8dfb0c96321",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "942c3f9715e5cad5e215",
+    "url": "/react-gh-pages/static/css/main.abbf07cf.chunk.css"
+  },
+  {
+    "revision": "4d389213f65d2487faa9",
+    "url": "/react-gh-pages/static/js/2.f71b9075.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.f71b9075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942c3f9715e5cad5e215",
+    "url": "/react-gh-pages/static/js/main.d367d283.chunk.js"
+  },
+  {
+    "revision": "3a4f73c321d9204569d7",
+    "url": "/react-gh-pages/static/js/runtime-main.1a4e0c80.js"
+  },
+  {
+    "revision": "1fd44bdf9274b331fe659004ccc71e63",
+    "url": "/react-gh-pages/static/media/bg-triangle.1fd44bdf.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/react-gh-pages/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/react-gh-pages/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/react-gh-pages/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/react-gh-pages/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/react-gh-pages/static/media/image-rules.5d74d25d.svg"
+  }
+]);
