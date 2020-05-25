59725a1f9d167306b3e37ca49ef04d8ca9386c5a
Agustin Zamar <agustinzamar33@gmail.com>
2020-05-24 21:07:07 -0300

    Updates

---
+++ precache-manifest.1031dd7d95201b3ae35422acfd127aae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38156439aef34fe609f56969b1ad6138",
+    "url": "/rock-paper-scissors-react/index.html"
+  },
+  {
+    "revision": "942c3f9715e5cad5e215",
+    "url": "/rock-paper-scissors-react/static/css/main.abbf07cf.chunk.css"
+  },
+  {
+    "revision": "0b7cc7e10a94d7a4a63e",
+    "url": "/rock-paper-scissors-react/static/js/2.487b325b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rock-paper-scissors-react/static/js/2.487b325b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942c3f9715e5cad5e215",
+    "url": "/rock-paper-scissors-react/static/js/main.d367d283.chunk.js"
+  },
+  {
+    "revision": "48723170453034b7face",
+    "url": "/rock-paper-scissors-react/static/js/runtime-main.bbb6e31e.js"
+  },
+  {
+    "revision": "1fd44bdf9274b331fe659004ccc71e63",
+    "url": "/rock-paper-scissors-react/static/media/bg-triangle.1fd44bdf.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/rock-paper-scissors-react/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/rock-paper-scissors-react/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/rock-paper-scissors-react/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/rock-paper-scissors-react/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/rock-paper-scissors-react/static/media/image-rules.5d74d25d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.debd82e7455b8ddd8b3b6ed579966327.js"
+  "/rock-paper-scissors-react/precache-manifest.1031dd7d95201b3ae35422acfd127aae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rock-paper-scissors-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
